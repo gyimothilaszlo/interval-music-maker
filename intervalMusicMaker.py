69af70d813768aeae512d6830c43f4d0a1c1a918
gyimothilaszlo <gyimothi.laszlo@gmail.com>
2017-09-25 09:28:18 +0200

    Added GUI.py

--- intervalMusicMaker.py
+++ intervalMusicMaker.py
@@ -1,44 +1,11 @@
 from os import listdir
-from random import shuffle
 from os.path import isfile, join
+from random import shuffle
 from tkinter import *
 from pydub import *
 from AudioMerger import *
+from GUI import *
 
-
-class GUI:
-	colors = ["#7f6012","#997316","#b28719", "#cc9a1d","#e5ad21","#ffc125"]
-	bgcolor = "AntiqueWhite2"
-	def __init__(self, master, worklengths):
-		self.repeats=[]
-		self.recoveryFactor = 0.5
-		self.entryfields=[]
-		self.master = master
-		self.master.configure(background=self.bgcolor)
-		
-		Label(text="Interval Music Maker", font=("Helvetica", 16), bg =self.bgcolor).grid(row=0,columnspan=2, padx = 5, pady = 5)
-		Label(text="Work lengths", font=("Verdana", 13), relief=RIDGE,fg="white", bg="saddle brown", width = 15).grid(row=1,column=0)
-		Label(text="Repeats", font=("Verdana", 13), relief=RIDGE,fg="white", bg="saddle brown", width=10).grid(row=1,column=1)
-		r = 2	
-		for wl, col in zip(worklengths, self.colors):
-			Label(text=str(wl) + " sec", bg=col,  font=("Verdana", 13), relief=RIDGE,width=15).grid(row=r,column=0)
-			e=Entry(relief=SUNKEN,width=10, font=("Courier New", 13))
-			e.grid(row=r,column=1)
-			self.entryfields.append(e)
-			r = r + 1
-		Label(text="recovery factor", fg="white", bg="saddle brown",  font=("Verdana", 13), relief=RIDGE,width=15).grid(row=r,column=0)
-		self.recovEntryField=Entry(relief=SUNKEN,width=10, font=("Courier New", 13))
-		self.recovEntryField.grid(row=r, column=1, pady = 10)
-		self.recovEntryField.insert(0, "0.5")
-		r = r + 1
-		Button(text="Create Music", height=1, width=14, fg="white", bg="RoyalBlue4", justify=CENTER, font=("Verdana", 13), command=self.callback).grid(row=r, columnspan = 2, padx=5, pady=5)
-
-	def callback(self):
-		self.repeats=[int(e.get()) if (len(e.get()) and e.get().isdigit()) else 0 for e in self.entryfields]
-		if (self.recovEntryField.get().replace('.','',1).isdigit()):
-			self.recoveryFactor = float(self.recovEntryField.get())
-		if  (sum(self.repeats) > 0):
-			self.master.destroy()
 	
 class IntervalMusicCreator:
 	ofsetSeconds = 10
